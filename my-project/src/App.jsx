64b306d011b7b724354eb645148fbe2960281500
Gaguchi <gaga9393@gmail.com>
2024-05-25 22:07:41 +0400

    update

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, Suspense, useRef } from 'react';
-import { PointLightHelper } from 'three';
+import { PointLightHelper, SpotLightHelper } from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
 import { useThree, Canvas, useFrame } from '@react-three/fiber';
@@ -14,8 +14,16 @@ function Sofa() {
     const dracoLoader = new DRACOLoader();
     dracoLoader.setDecoderPath('/node_modules/three/examples/jsm/libs/draco/');
     loader.setDRACOLoader(dracoLoader);
-
-    loader.load('assets/sofa.glb', setGltf, undefined, error => {
+  
+    loader.load('assets/sofa.glb', (gltf) => {
+      gltf.scene.traverse((child) => {
+        if (child.isMesh) {
+          child.castShadow = true;
+          child.receiveShadow = true;
+        }
+      });
+      setGltf(gltf);
+    }, undefined, error => {
       console.error('Error loading GLTF:', error);
     });
   }, []);
@@ -33,6 +41,7 @@ function Sofa() {
       ref={ref}
       object={gltf.scene} 
       position={[0, -0.3, 3]} // Set the position to the origin
+      rotation={[Math.PI / 8, 0, 0]} // Rotate the scene 90 degrees around the x-axis
     />
   ) : null;
 }
@@ -47,13 +56,19 @@ function Table() {
     const dracoLoader = new DRACOLoader();
     dracoLoader.setDecoderPath('/node_modules/three/examples/jsm/libs/draco/');
     loader.setDRACOLoader(dracoLoader);
-
-    loader.load('assets/table.glb', setGltf, undefined, error => {
+  
+    loader.load('assets/table.glb', (gltf) => {
+      gltf.scene.traverse((child) => {
+        if (child.isMesh) {
+          child.castShadow = true;
+          child.receiveShadow = true;
+        }
+      });
+      setGltf(gltf);
+    }, undefined, error => {
       console.error('Error loading GLTF:', error);
     });
   }, []);
-
-  console.log('GLTF Object:', gltf);
 
   useFrame(() => {
     if (ref.current) {
@@ -66,6 +81,7 @@ function Table() {
       ref={ref}
       object={gltf.scene} 
       position={[0, -0.3, 3]} // Set the position to the origin
+      rotation={[Math.PI / 8, 0, 0]} // Rotate the scene 90 degrees around the x-axis
     />
   ) : null;
 }
@@ -75,13 +91,22 @@ function Chair() {
   const [gltf, setGltf] = useState(null);
   const ref = useRef();
 
+
   useEffect(() => {
     const loader = new GLTFLoader();
     const dracoLoader = new DRACOLoader();
     dracoLoader.setDecoderPath('/node_modules/three/examples/jsm/libs/draco/');
     loader.setDRACOLoader(dracoLoader);
-
-    loader.load('assets/chair.glb', setGltf, undefined, error => {
+  
+    loader.load('assets/chair.glb', (gltf) => {
+      gltf.scene.traverse((child) => {
+        if (child.isMesh) {
+          child.castShadow = true;
+          child.receiveShadow = true;
+        }
+      });
+      setGltf(gltf);
+    }, undefined, error => {
       console.error('Error loading GLTF:', error);
     });
   }, []);
@@ -99,22 +124,28 @@ function Chair() {
       ref={ref}
       object={gltf.scene} 
       position={[0, -0.3, 3]} // Set the position to the origin
+      rotation={[Math.PI / 8, 0, 0]} // Rotate the scene 90 degrees around the x-axis
     />
   ) : null;
 }
 
+
+
 function Lights() {
   const { scene } = useThree();
+  const spotLightRef = useRef();
   const pointLightRef = useRef();
+  let helper;
 
   useEffect(() => {
-    if (pointLightRef.current) {
-      const helper = new PointLightHelper(pointLightRef.current);
+    if (spotLightRef.current) {
+      helper = new SpotLightHelper(spotLightRef.current);
+      // scene.add(helper);
     }
 
     return () => {
-      if (pointLightRef.current) {
-        const helper = new PointLightHelper(pointLightRef.current);
+      if (helper) {
+        scene.remove(helper);
       }
     };
   }, [scene]);
@@ -122,6 +153,17 @@ function Lights() {
   return (
     <>
       <ambientLight intensity={2} />
+      <spotLight 
+        ref={spotLightRef} 
+        position={[0, 1, 5]} 
+        intensity={5} 
+        angle={Math.PI/2} 
+        penumbra={1} 
+        castShadow={true} 
+        shadow-mapSize-width={2048} 
+        shadow-mapSize-height={2048} 
+        shadow-bias={-0.005} 
+      />
       <pointLight ref={pointLightRef} position={[0.5, -0.5, 5]} />
     </>
   );
@@ -136,7 +178,7 @@ function App() {
 						<div id="content" className="site-content" role="main">
 							<section className="section m-b-0">
 								<div className="block block-sliders layout-6 nav-vertical">
-                  <div className="slick-sliders" data-autoplay="false" data-dots="true" data-nav="false" data-columns4="1" data-columns3="1" data-columns2="1" data-columns1="1" data-columns1440="1" data-columns="1">
+                  <div className="slick-sliders" data-autoplay="false" data-dots="true" data-nav="false"  data-columns4="1" data-columns3="1" data-columns2="1" data-columns1="1" data-columns1440="1" data-columns="1">
                     <div className="item slick-slide">
                       <div className="item-content">
                         <div className="content-image">
@@ -189,7 +231,7 @@ function App() {
                           <div className="content">
                             <div className="subtitle-slider">20%OFF.END MONDAY</div>
                             <h2 className="title-slider">Outline Studio Chair</h2>
-                            <a className="button-slider button-white" href="shop-grid-left.html">SHOP NOW</a>
+                            <a className="button-slider button-white" href="shop-details.html">SHOP NOW</a>
                           </div>
                         </div>
                       </div>
